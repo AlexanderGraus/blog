a16d77952ea4ab98fa504863945f92ce5003964c
Alexander Graus <alegraus31@gmail.com>
2021-03-24 00:59:28 -0300

    Agregadas vistas y plantilla para el detalle del post del blog asi como tambien css para el sitio

--- blog/urls.py
+++ blog/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('',views.post_list, name='post_list'),
+    path('post/<int:pk>/', views.post_detail, name='post_detail'), 
+    # django va a pasar un int con el nombre pk, este lo va a sacar de la url que ponga el usuario
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render
 from django.utils import timezone
 from .models import Post
+from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
 
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request,'blog/post_list.html',{"posts":posts})
+
+def post_detail(request,pk): #la variable se tiene que llamar exactamente pk porque asi la defini en urls 
+    post = get_object_or_404(Post, pk = pk)
+    return render(request,'blog/post_detail.html',{'post':post})
+    # mando a renderizar la vista y le envio el post
